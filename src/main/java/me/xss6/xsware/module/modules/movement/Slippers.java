4c05b86f9fca986ffe957c7c256e72c5608de93f
Chocolate Milk <96555086+Wylx-32k@users.noreply.github.com>
2022-08-11 13:38:30 -0400

    Update Slippers.java

--- src/main/java/me/xss6/xsware/module/modules/movement/Slippers.java
+++ src/main/java/me/xss6/xsware/module/modules/movement/Slippers.java
@@ -2,7 +2,7 @@ package me.xss6.xsware.module.modules.movement;
 
 import me.xss6.xsware.module.Module;
 
-@Module.Registration(name = "Slippers", description = "Oh no I hope I don't fall", category = Hack.Category.MOVEMENT, isListening = false)
+@Module.Registration(name = "Slippers", description = "Oh no I hope I don't fall", category = Module.Category.MOVEMENT, isListening = false)
 public class Slippers extends Module{
     @Override
     public void onTick() {
