b1104e5014a2bf6e219cde9aec5082aa6695b90a
Chocolate Milk <96555086+Wylx-32k@users.noreply.github.com>
2022-08-11 13:06:28 -0400

    Added InfiniteJump

--- src/main/java/me/xss6/xsware/hack/hacks/movement/InfiniteJump.java
+++ src/main/java/me/xss6/xsware/hack/hacks/movement/InfiniteJump.java
@@ -2,7 +2,7 @@ package me.xss6.xsware.hack.hacks.movement;
 
 import me.xss6.xsware.hack.Hack;
 
-@Hack.Registration(name = "InfiniteJump", description = "Jump.. infinitely", category = Hack.Category.MOVEMENT, isListening = false)
+@Hack.Registration(name = "Infinite Jump", description = "Jump.. infinitely", category = Hack.Category.MOVEMENT, isListening = false)
 public class InfiniteJump extends Hack{
     @Override
     public void onTick() {
