22ac6db705120d6da8379aab8c76e136e53e6e28
Chocolate Milk <96555086+Wylx-32k@users.noreply.github.com>
2022-08-11 13:30:06 -0400

    Update InfiniteJump.java
    Hack to Module

--- src/main/java/me/xss6/xsware/module/modules/movement/InfiniteJump.java
+++ src/main/java/me/xss6/xsware/module/modules/movement/InfiniteJump.java
@@ -1,9 +1,9 @@
-package me.xss6.xsware.hack.hacks.movement;
+package me.xss6.xsware.module.modules.movement;
 
-import me.xss6.xsware.hack.Hack;
+import me.xss6.xsware.module.Module;
 
-@Hack.Registration(name = "Infinite Jump", description = "Jump.. infinitely", category = Hack.Category.MOVEMENT, isListening = false)
-public class InfiniteJump extends Hack{
+@Module.Registration(name = "Infinite Jump", description = "Jump.. infinitely", category = Hack.Category.MOVEMENT, isListening = false)
+public class InfiniteJump extends Module{
     @Override
     public void onTick() {
         mc.player.onGround = true;
